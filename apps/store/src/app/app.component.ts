38eed61a5e7ba6d58906201bb62381631f10cc49
Cassandra Curcio <cassandra.curcio@gmail.com>
2023-05-19 17:16:04 -0400

    adjust design

--- apps/store/src/app/app.component.ts
+++ apps/store/src/app/app.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+// import { RouterModule } from '@angular/router';
+// import { NxWelcomeComponent } from './nx-welcome.component';
+import { exampleProducts } from '@myorg/products';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  // imports: [NxWelcomeComponent, RouterModule],
   selector: 'myorg-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  template: `<myorg-banner title="Welcome to the store!"></myorg-banner>
+    <ul>
+      <li *ngFor="let product of products">
+        <strong>{{ product.name }}</strong> Price: {{ product.price }}
+      </li>
+    </ul> `,
+  // templateUrl: './app.component.html',
+  // styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'store';
+  products = exampleProducts;
 }
---
+++ apps/store/src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { CommonUiModule } from '@myorg/common-ui';
+
+import { AppComponent } from './app.component';
+import { NxWelcomeComponent } from './nx-welcome.component';
+
+@NgModule({
+  declarations: [AppComponent, NxWelcomeComponent],
+  imports: [BrowserModule, CommonUiModule],
+  // imports: [BrowserModule, CommonUiModule, AppComponent, NxWelcomeComponent, AppComponent],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
